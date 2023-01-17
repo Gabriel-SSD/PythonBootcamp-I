f063e39aa0f4258aa975333e37349a1405f964e6
Gabriel Soares <desmaraisgss@gmail.com>
2023-01-17 19:48:24 -0300

    solucionado

--- US States Quiz/main.py
+++ US States Quiz/main.py
@@ -1,18 +1,38 @@
 import turtle
 import pandas
-screen = turtle.Screen()
-screen.title("U.S States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+
+def screen_configs():
+    scr = turtle.Screen()
+    scr.title("U.S States Game")
+    image = "blank_states_img.gif"
+    scr.addshape(image)
+    turtle.shape(image)
+    return scr
+def check_answer (ans):
+    global condition
+    if ans in states:
+        condition += 1
+        sdata = data[data.state == ans]
+        write(ans, sdata)
+        states.remove(ans)
+    elif ans == 'Exit':
+        condition = 51
+def write (ans, sdata):
+    text = turtle.Turtle()
+    text.penup()
+    text.hideturtle()
+    text.goto(int(sdata.x), int(sdata.y))
+    text.write(ans)
+screen = screen_configs()
 condition = 0
 data = pandas.read_csv("50_states.csv")
-def check_answer (ans):
-    if data[data["state"]] == ans:
-        pass
-        # escrever nome na imagem
-        # incrementar 1 estado correto
+states = data["state"].to_list()
+
 while condition < 50:
-    print(data[data.state] == "Colorado")
-    #answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-    #check_answer(answer_state)
+    answer_state = screen.textinput(title=f"{condition}/50", prompt="What's another state's name?").title()
+    check_answer(answer_state)
+if condition == 51:
+    missing = pandas.DataFrame(states)
+    missing.to_csv("missing_states.csv")
+else:
+    print("You win!")

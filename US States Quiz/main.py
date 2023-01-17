1a13728a76f2db4661b4f77cd10c967430269e51
Gabriel Soares <desmaraisgss@gmail.com>
2023-01-17 12:46:28 -0300

    main func + check_ans

--- US States Quiz/main.py
+++ US States Quiz/main.py
@@ -1,10 +1,18 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
-
-screen.exitonclick()
+condition = 0
+data = pandas.read_csv("50_states.csv")
+def check_answer (ans):
+    if data[data["state"]] == ans:
+        pass
+        # escrever nome na imagem
+        # incrementar 1 estado correto
+while condition < 50:
+    print(data[data.state] == "Colorado")
+    #answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    #check_answer(answer_state)
